8ab7891d58ba2393bb3c4fccbb2a3a5b9f693fae
YitingLiu <yl3799@nyu.edu>
2017-04-16 18:11:00 -0400

    repeat

--- js/repeat.js
+++ js/repeat.js
@@ -19,6 +19,10 @@ var s1=function(p){
     p.drawRect3(w *11/ 12, h /2, dia);
   };
 
+  p.windowResized=function() {
+    p.setup();
+  }
+
   p.drawCircle1=function(x, y, d) {
     p.ellipse(x, y, d);
     if (d > 20) {
@@ -76,3 +80,112 @@ var s1=function(p){
   }
 }
 var myp5 = new p5(s1, 'c1');
+
+var s2=function(p){
+  var w;
+  p.setup=function(){
+    w=window.innerWidth/2-50;
+    p.createCanvas(w, w);
+    p.translate(p.width / 2, p.height);
+    p.branch(w/4);
+  };
+
+  p.windowResized=function() {
+    p.setup();
+  };
+
+  p.branch=function(len){
+    p.line(0, 0, 0, -len);
+    p.translate(0, -len);
+    len *= 0.7;
+    if (len > 2) {
+      var n = p.floor(p.random(1.5, 2.5));
+      for (var i = 0; i < n; i++) {
+        p.push();
+        p.rotate(p.random(-p.PI / 3, p.PI / 3));
+        p.branch(len);
+        p.pop();
+      }
+    } 
+  };
+
+  p.mousePressed=function() {
+    p.background(255);
+    p.translate(p.width / 2, p.height);
+    p.branch(w/4);
+  }
+}
+var myp5 = new p5(s2, 'c2');
+
+
+var s3=function(p){
+    var w;
+  p.setup=function(){
+    w=window.innerWidth/2-50;
+    p.createCanvas(w, w);    p.push();
+    p.translate(p.width / 2, p.height);
+    p.branch(w/4);
+    p.pop();
+  }
+  p.windowResized=function() {
+    p.setup();
+  };
+
+  p.mousePressed=function() {
+    p.setup();
+  }
+
+  p.branch=function(len){
+    p.stroke(0, len);
+    p.line(0, 0, 0, -len);
+    p.translate(0, -len);
+    len *= 0.7;
+    if (len > 2) {
+      var n = p.floor(p.random(1.5, 5.5));
+      for (var i = 0; i < n; i++) {
+        p.push();
+        p.rotate(p.randomGaussian(0,0.5));
+        p.branch(len);
+        p.pop();
+      }
+    }
+  }
+}
+
+var myp5 = new p5(s3, 'c3');
+
+// var s4=function(p){
+//   var w;
+//   p.setup=function(){
+//     w=window.innerWidth/2-50;
+//     p.createCanvas(w, w);
+//     p.translate(p.width / 2, p.height);
+//     p.branch(w/4);
+//   };
+
+//   p.windowResized=function() {
+//     p.setup();
+//   };
+
+//   p.branch=function(len){
+//     p.line(0, 0, 0, -len);
+//     p.translate(0, -len);
+//     len *= 0.7;
+//     if (len > 2) {
+//       var n = p.floor(p.random(1.5, 2.5));
+//       for (var i = 0; i < n; i++) {
+//         p.push();
+//         p.rotate(p.random(-p.PI / 3, p.PI / 3));
+//         p.branch(len);
+//         p.pop();
+//       }
+//     } 
+//   };
+
+//   p.mousePressed=function() {
+//     p.translate(p.width / 2, p.height);
+//     p.branch(w/4);
+//   }
+// }
+
+// var myp5 = new p5(s4, 'c4');
