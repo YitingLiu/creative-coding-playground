2a3a47546c8cc7eccdeb1a3c39acd7c234a6d0ce
Yiting Liu <yitingliu@Yitings-MacBook-Pro.local>
2020-04-18 16:48:05 +0800

    flying test

--- threejs/flying/js/main.js
+++ threejs/flying/js/main.js
@@ -32,8 +32,8 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(renderer.domElement);
 
 //camera
-camera.position.set(25,5,0);
-camera.lookAt(new THREE.Vector3(0,5,0));
+camera.position.set(50,15,0);
+camera.lookAt(new THREE.Vector3(0,3,0));
 
 // camera.position.set(0, 2, 25); //sheep front
 // camera.position.set(8, 1, 0); //sheep left
@@ -50,10 +50,10 @@ var keyLight = new THREE.DirectionalLight(col_light, 0.6);
 keyLight.position.set(20, 30, 10);
 keyLight.castShadow = true;
 
-keyLight.shadow.camera.left = -5; //default:-5
-keyLight.shadow.camera.right = 25; //default:5
-keyLight.shadow.camera.top = 20; // default:5
-keyLight.shadow.camera.bottom = -20; //default:-5
+keyLight.shadow.camera.left = -10; //default:-5
+keyLight.shadow.camera.right = 100; //default:5
+keyLight.shadow.camera.top = 100; // default:5
+keyLight.shadow.camera.bottom = -100; //default:-5
 
 // var shadowHelper = new THREE.CameraHelper( keyLight.shadow.camera );
 // scene.add( shadowHelper );
@@ -80,6 +80,10 @@ var mat_yellow=new THREE.MeshLambertMaterial({color:0xfeb42b});
 var mat_dark=new THREE.MeshLambertMaterial({color:0x5a6e6c});
 var mat_brown=new THREE.MeshLambertMaterial({color:0xa3785f});
 var mat_stone=new THREE.MeshLambertMaterial({color:0x9EAEAC});
+
+//-------------------------------------islands-------------------------------------
+var island=new THREE.Group();
+
 //-------------------------------------ground-------------------------------------
 var layers = [];
 var ground = new THREE.Group();
@@ -107,7 +111,7 @@ base.scale.x=layers[0].scale.x;
 base.position.y=-5;
 ground.add(base);
 
-scene.add(ground);
+island.add(ground);
 
 
 //-------------------------------------trees-------------------------------------
@@ -189,13 +193,13 @@ leaf_1.position.set(trunk.position.x-.2,6,trunk.position.z-1);
 tree.add(leaf_1);
 tree.rotation.y=-pi/12;
 tree.position.set(-2,0,-2);
-scene.add(tree);
+island.add(tree);
 
 var tree_1=tree.clone();
 tree_1.scale.set(.8,.8,.8);
 tree_1.position.set(-1,0,-5);
 tree_1.rotation.y=-pi/5;
-scene.add(tree_1);
+island.add(tree_1);
 
 var tree_2=tree.clone();
 tree_2.scale.set(.7,.7,.7);
@@ -205,14 +209,14 @@ tree_2.children[2].rotation.x=-pi/3;
 tree_2.children[2].position.z=trunk.position.z-1;
 tree_2.children[3].rotation.x=pi/3;
 tree_2.children[3].position.z=trunk.position.z+1;
-scene.add(tree_2);
+island.add(tree_2);
 
 //-------------------------------------stone-------------------------------------
 var geo_stone=new THREE.DodecahedronGeometry(1,0);
 var stone=[];
 for(var i=0;i<2;i++){
   stone[i]=new THREE.Mesh(geo_stone,mat_stone);
-  scene.add(stone[i]);
+  island.add(stone[i]);
   stone[i].castShadow=true;
 }
 stone[0].rotation.set(0,12,pi/2);
@@ -335,7 +339,7 @@ eyeballs[1].position.x+=.05;
 sheep.position.set(4.8,-.2,-1);
 sheep.scale.set(.8,.8,.8);
 sheep.rotation.set(0,pi/4,0);
-scene.add(sheep);
+island.add(sheep);
 
 //fence
 var fence=new THREE.Group();
@@ -364,13 +368,166 @@ wood[2].rotation.y=pi/32;
 
 fence.position.set(3,0,2);
 fence.rotation.y=pi/5;
-scene.add(fence);
+island.add(fence);
+
+// scene.add(island);
+
+var islands=[];
+for(var i=0;i<6;i++){
+  islands[i]=island.clone();
+  scene.add(islands[i]);
+  islands[i].position.z=-20*i;
+}
+
+
+//-------------------------------------airplane-------------------------------------
+
+
+// camera.position.set(20, 1, 10); //airplane front
+// camera.position.set(30, 1, 0); //airplane left
+// camera.position.set(20, 10, 0); //airplane top
+// camera.lookAt(new THREE.Vector3(20, 0, 0));
+
+
+var mat_purple=new THREE.MeshLambertMaterial({color:0x8086E3});
+
+var box=new THREE.BoxGeometry(1,1,1);
+var airplane=new THREE.Group();
+
+var airplane_body=new THREE.Mesh(box,mat_purple);
+airplane_body.scale.set(1,1,2.5);
+airplane.add(airplane_body);
+
+var airplane_cockpit=new THREE.Mesh(box,mat_purple);
+airplane_cockpit.position.set(0,.8,0);
+airplane_cockpit.scale.set(.9,.9,.9);
+airplane.add(airplane_cockpit);
+
+var airplane_wing=new THREE.Mesh(box,mat_purple);
+airplane_wing.scale.set(4,.15,1);
+airplane.add(airplane_wing);
+
+var airpalne_tail=new THREE.Mesh(box,mat_purple);
+var a=Math.sqrt(.5*.5+.75*.75);
+airpalne_tail.position.set(0,.5-a*Math.sin(pi/4+pi+Math.atan(.75/.5))-1,1.75-.5+a*Math.cos(pi/4+pi+Math.atan(.75/.5)));
+airpalne_tail.scale.set(1,1.5,1);
+airpalne_tail.rotation.set(pi/4,0,0);
+airplane.add(airpalne_tail);
+
+airplane.position.set(20,0,0);
+scene.add(airplane);
+
+
+//gas
+var Gas=function(x,y,z,speed){
+  this.geometry=new THREE.DodecahedronGeometry(.5);
+  this.gas=new THREE.Mesh(this.geometry,mat_grey);
+  scene.add(this.gas);
+  this.gas.position.x=x;
+  this.gas.position.y=y;
+  this.gas.position.z=z+.5*Math.random();
+  this.lifespan=50;
+  this.speed=speed;
+
+  this.update=function(){
+    this.gas.scale.x*=.97;
+    this.gas.scale.y*=.97;
+    this.gas.scale.z*=.97;
+    this.gas.position.z-=this.speed;
+    this.gas.position.y-=.05*Math.random();
+    this.lifespan--;
+  }
+};
+
+var gases=[];
+
+
+//-------------------------------------Control-------------------------------------
+let speed=.05;
+let acc=.0005;
+let res=.0005;
+
+//height
+let speed_lift=0;
+let acc_lift=0;
+let gravity=.005;
+
+document.addEventListener("keydown",function(e){
+  if(e.key=="ArrowRight"){
+    acc+=.02;
+    acc_lift+=.01;
+  }
+  if(e.key=="ArrowLeft") {
+    acc-=.02;
+  }
+})
 
 
 
+
+
+
+
+
+var count=0;
 //render
 var render = function () {
+  speed+=acc;
+  acc=.0005;
+  if(speed>0) speed-=res;
+  else speed=0;
+  airplane.position.z-=speed;
+  camera.position.z=airplane.position.z;
+
+  speed_lift+=acc_lift;
+  acc_lift=0;
+  speed_lift-=gravity;
+  if(airplane.position.y<0) {
+    var buffer_bottom=-.2*speed_lift;
+    buffer_bottom=Math.max(.02,buffer_bottom);
+    speed_lift+=buffer_bottom;
+  }
+  airplane.position.y+=speed_lift;
+  if(airplane.position.z<-100) airplane.position.z=0;
+
+  //gas
+  count++;
+  if(count%8==0) gases.push(new Gas(airplane.position.x,airplane.position.y,airplane.position.z+2,speed*.6));
+
+  for(var i=0;i<gases.length;i++){
+    gases[i].update();
+    if(gases[i].lifespan<0){
+      scene.remove(scene.getObjectById(gases[i].gas.id));
+      gases.splice(i,1);
+    }
+  }
+  console.log(gases.length)
+
+
   requestAnimationFrame(render);
   renderer.render(scene, camera);
 };
 render();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
